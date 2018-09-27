c22600be95594a30a5415c1d6bd9596fad425077
Aaqib Anees <aaqibanees@hotmail.co.uk>
2018-09-27 21:03:35 +0500

    sidebar added

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <!--[if IE]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
@@ -12,7 +12,18 @@
 
 <body>
 
+    <h2>Hello World</h2>
 
+    <p class="hello">Hello this is the first paragraph of this file</p>
+
+    <div class="box">
+
+        <p>Hello this is first test</p>
+
+        <p>Hello this is second test</p>
+
+        <p>Hello this is third test</p>
+    </div>
 
 </body>
 </html>
